8ea1734e97a228804b8a47e54f55a904014e9d57
Engineer AbidH <abidali70sb@gmail.com>
2025-10-18 23:29:36 +0200

    removed last comment

--- test.py
+++ test.py
@@ -4,5 +4,4 @@ print(a and b)
 # this will print False because a is True and b is False so the output will be False
 print(a or b)
 # this will print True because a is True and b is False so the output will be True
-# recently edited lines
 
